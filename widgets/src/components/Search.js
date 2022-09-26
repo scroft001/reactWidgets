58492a7c6a37efd7e97cb16a82997aff552513eb
Steven Croft <scroft001@gmail.com>
2022-09-25 21:56:37 -0700

    Make the API request useEffect hook

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
     const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php?', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+        };
+        search();
+    }, [term]);
 
     return (
         <div>
@@ -12,6 +28,7 @@ const Search = () => {
                     <input className='input' value={term} onChange={(e) => setTerm(e.target.value)}/>
                 </div>
             </div>
+            
         </div>
     )
 }
