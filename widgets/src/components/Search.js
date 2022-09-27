9c53295c4c770330692eb9145e4d28af5e524774
Steven Croft <scroft001@gmail.com>
2022-09-26 21:50:32 -0700

    delayed api call

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,20 +20,28 @@ const Search = () => {
 
             setResults(data.query.search);
         };
-        if(term){
-            search();
-        }
+        const timeoutId = setTimeout(() => {
+            if(term){
+                search();
+            }
+        }, 500)
         
+        return(()=> {
+            clearTimeout(timeoutId);
+        })
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a className='ui button' href={`https://en.wikipedia.org?curid=${result.pageid}`} target='_blank' rel='noreferrer'>Go</a>
+                </div>
                 <div className='content'>
                     <div className='header'>
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         )
