48aa0e5be16e3694e905cd994b5694a10135dbfe
Steven Croft <scroft001@gmail.com>
2022-12-01 17:06:15 -0800

    Added dropdown component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
     {
@@ -21,7 +22,8 @@ export default () => {
     return(
         <div>
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown />
         </div>
         
     )
